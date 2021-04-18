ba493d016e0d263dd82887c1a3e837a81b4c327f
gherardovarando <gherardo.varando@gmail.com>
2021-04-18 12:06:24 +0200

    dynamic programming order searach

--- search_order.R
+++ search_order.R
@@ -1,4 +1,4 @@
-all_orders <- function(data, alg = stages_bhc, start = full){
+all_orders <- function(data, alg = stages_bhc){
   if (is.data.frame(data)){
     vv <- colnames(data)
   }
@@ -6,7 +6,7 @@ all_orders <- function(data, alg = stages_bhc, start = full){
     vv <- names(dimnames(data))
   }
   allres <- combinat::permn(vv, fun = function(order){
-    alg(start(data, order =  order))
+    alg(full(data, order =  order, join_unobserved = FALSE))
   })    
   scores <- sapply(allres, BIC)
   return(allres)
@@ -49,11 +49,11 @@ search_greedy <- function(data, alg = stages_bhc, score = BIC){
     vs <- names(dimnames(data))
   }
   ## initialize best
-  best <- full(data, order = vs[1])
+  best <- full(data, order = vs[1], join_unobserved = FALSE)
   ## check all other possible first variable
   if (length(vs) < 2) return(best)
   for (v in vs){
-    tmp <- full(data, order = v)
+    tmp <- full(data, order = v, join_unobserved = FALSE)
     #print(score(tmp))
     if (score(tmp) < score(best)){
       best <- tmp
@@ -79,3 +79,44 @@ search_greedy <- function(data, alg = stages_bhc, score = BIC){
   return(object)
 }
 
+bls <- function(data, left, new, alg, score = BIC){
+  m <- full(data = data, order = left)
+  s <- score(m)
+  m <- sevt_add(m, new, data)
+  return(score(m) - s)
+}
+
+search_dynamic <- function(data, alg = stages_bhc, score = BIC){
+  if (is.data.frame(data)){
+    vs <- colnames(data)
+  }
+  if (is.table(data)){
+    vs <- names(dimnames(data))
+  }
+  ## initialize scores with 1 variables
+  scores <- sapply(vs, FUN = function(vv){
+    score(full(data, order = vv, join_unobserved = FALSE))
+  },USE.NAMES = TRUE )
+  sinks <- vs
+  names(sinks) <- vs
+  for (i in seq_along(vs)[-1]){
+    sets <- combn(vs,i)
+    tmp <- apply(sets, MARGIN = 2, FUN = function(W){
+      sapply(W, function(v){
+        scores[paste(W[W!=v],collapse="-")] + bls(data, W[W!=v], v, alg, score)
+      })
+    })
+    new_sinks <- sapply(seq(ncol(sets)), function(i) sets[which.min(tmp[,i]),i])
+    nam <- apply(sets, MARGIN = 2, FUN = paste, collapse = "-")
+    sinks[nam] <- new_sinks
+    scores[nam] <- apply(tmp, 2, min)
+  }
+  left <- paste(vs, collapse = "-")
+  order <- c()
+  for (i in length(vs):1){
+    order[i] <- sinks[left]
+    left <- paste(vs[!(vs %in% order)], collapse = "-")
+  }
+  object <- alg(full(data, order = order, join_unobserved = FALSE))
+  return(object)
+}
