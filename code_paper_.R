cac80fd992f71c084d1edb3cdc601e44e04f67fb
gherardo varando <gherardo.varando@gmail.com>
2021-04-19 15:03:22 +0200

    update

--- code_paper_.R
+++ code_paper_.R
@@ -35,25 +35,25 @@ datasets <- list(
 )
 
 
-
-
-######## comparisons 
-
-results <- lapply(datasets, function(data){
+experiment <- function(data, lambda = 0, r_train = 1, seed = 0){
+  set.seed(seed)
+  ix <- sample(seq(nrow(data)), size = nrow(data)*r_train)
+  train <- data[ix,]
+  test <-  data[-ix,]
   print(colnames(data))
-  time.dag <- system.time(dag_hc <- bnlearn::hc(data))
-  dag_fitted <- bn.fit(dag_hc, data = data)
-  sevt_from_dag <- sevt_fit(as_sevt(dag_fitted), data = data, lambda = 0)
+  time.dag <- system.time(dag_hc <- bnlearn::hc(train))
+  dag_fitted <- bn.fit(dag_hc, data = train)
+  sevt_from_dag <- sevt_fit(as_sevt(dag_fitted), data = train, lambda = lambda)
   order <- bnlearn::node.ordering(dag_hc)
-  time.simple_marginal <- system.time(marginal <- simple_marginal(full(data, order, join_unobserved = FALSE, lambda = 0)))
-  time.simple_total <- system.time(total <- simple_total_bhc(full(data, order, join_unobserved = FALSE, lambda = 0)))
-  time.sevt_bhc <- system.time(sevt_bhc <- stages_bhc(stndnaming(stages_bhc(full(data, order, join_unobserved = TRUE, lambda = 0)), ignore = FALSE)))
+  time.simple_marginal <- system.time(marginal <- simple_marginal(full(train, order, join_unobserved = FALSE, lambda = lambda)))
+  time.simple_total <- system.time(total <- simple_total_bhc(full(train, order, join_unobserved = FALSE, lambda = lambda)))
+  time.sevt_bhc <- system.time(sevt_bhc <- stages_bhc(stndnaming(stages_bhc(full(train, order, join_unobserved = TRUE, lambda = lambda)), ignore = FALSE)))
   time.simplify <- system.time(simplified <- simplify(sevt_bhc))
-  time.greedy_marginal <- system.time(greedy_marginal <- search_greedy(data = data,
-                                                                       alg = simple_marginal, join_unobserved = FALSE, lambda = 0))
+  time.greedy_marginal <- system.time(greedy_marginal <- search_greedy(data = train,
+                                                                       alg = simple_marginal, join_unobserved = FALSE, lambda = lambda))
   if (ncol(data)<7){
-    time.all_marginal <- system.time(all_marginal <- search_all(data, alg = simple_marginal, join_unobserved = FALSE, lambda = 0)) 
-    time.all_total <- system.time(all_total <- search_all(data, alg = simple_total_bhc, join_unobserved = FALSE, lambda = 0))
+    time.all_marginal <- system.time(all_marginal <- search_all(train, alg = simple_marginal, join_unobserved = FALSE, lambda = lambda)) 
+    time.all_total <- system.time(all_total <- search_all(train, alg = simple_total_bhc, join_unobserved = FALSE, lambda = lambda))
   }else{
     time.all_marginal <- time.all_total <- NA
     all_marginal <- all_total <- NA
@@ -76,8 +76,15 @@ results <- lapply(datasets, function(data){
                 all_marginal = time.all_marginal,
                 all_total = time.all_total
               ),
-              dag = dag_hc))
-})
+              dag = dag_hc,
+              train = train,
+              test = test))
+}
+
+
+######## comparisons 
+
+results <- lapply(datasets, experiment, lambda = 0, r_train = 1)
 
 table.BIC <- t(as.data.frame(lapply(results, function(x) sapply(x$models, function(m) {
   if (length(m) == 1) return(NA) else 
@@ -85,7 +92,6 @@ table.BIC <- t(as.data.frame(lapply(results, function(x) sapply(x$models, functi
 }))))
 View(table.BIC)
 saveRDS(table.BIC, file = "tableBIC.rds")
-
 
 table.time <- t(as.data.frame(lapply(results, function(x) sapply(x$time, function(m) {
   if (length(m) == 1) return(NA) else 
@@ -107,12 +113,26 @@ table.stages <- t(as.data.frame(lapply(results, function(x) sapply(x$models, fun
 }))))
 saveRDS(table.stages, file = "tablestages.rds")
 View(table.stages)
+
+########## train-test
+
+results_1 <- lapply(datasets, experiment, lambda = 1, r_train = 0.5)
+
+table.logLik <- t(as.data.frame(lapply(results_1, function(x) sapply(x$models, function(m) {
+  if (length(m) == 1) return(NA) else 
+    sum(stagedtrees::prob(m, x$test, log = TRUE)) 
+}))))
+saveRDS(table.logLik, file = "tableloglik.rds")
+View(table.logLik)
+
+
 ## CHDS PLOTS
 plot(results$chds$dag)
 plot(results$chds$models$simple_total)
 ceg.plot(results$chds$models$simple_total)
 
 ## CORONARY PLOTS (TO DO)
-plot(dag_cor)
-plot(total_cor)
-ceg.plot(total_cor)
+plot(results$coronary$dag)
+plot(results$coronary$models$simple_total)
+ceg.plot(results$coronary$models$simple_total)
+
